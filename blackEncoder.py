789f2dc9140a14856281a81d3d22a9c805acc33e
salva <salva.nst@gmail.com>
2017-06-25 12:14:42 +0200

    Update blackEncoder.py

--- blackEncoder.py
+++ blackEncoder.py
@@ -131,7 +131,7 @@ ffmpeg \
 
 
 class Window(QtGui.QMainWindow):
-    def __init__(self) -> object:
+    def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 460, 400)
         self.setWindowTitle("blackEncoder")
